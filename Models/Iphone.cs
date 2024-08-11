6bb0b40bdb6f72a90b23a6f0fdb9bea518a1cb18
Glauco Diogo <glaucodiogoalves@gmail.com>
2024-08-11 02:14:39 -0300

    cumprindo o desafio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        List<string> Aplicativos = new List<string>();
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Aplicativos.Add(nomeApp);
+            Console.WriteLine($"Instalando {nomeApp}...");
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        List<string> Aplicativos = new List<string>();
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Aplicativos.Add(nomeApp);
+            Console.WriteLine($"Instalando {nomeApp}...");
+        }
     }
 }
